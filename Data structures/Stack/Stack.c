eb7dd6c5513cff42453707a844419a8dc898666b
mohamed magdy <mhe778080@gmail.com>
2023-03-04 22:58:31 +0200

    New add

---
+++ Data structures/Stack/Stack.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include<stdio.h>
+#include"STDTYPE.h"
+#include"Stack_Fun.h"
+
+int main()
+{
+Stack S;
+Stack *ptr=&S;
+u64 e  ,x,cond=1; // cond to push or pop a member ....e is a member
+Stack_Ctreate (&S);
+
+while(cond!=0)
+{
+
+if(isfull (&S))
+{
+ break;
+}
+else 
+{
+printf("Enter the member:");
+scanf("%d",&e);
+ push(e,&S);
+
+}
+scanf("%d",&cond);
+}
+
+printf("Pop...?");
+
+scanf("%d",&cond);
+while(cond!=0)
+{
+if(isempty (&S))
+{
+break;
+}
+else
+{
+pop(e,&S);
+}
+scanf("%d",&cond);
+}
+
+
+
+ Clear_Stack(&S); 
+
+ Stack_Display(&S);
+
+ x = Stack_Size(&S);
+ printf("Size=%d" ,x);
+
+
+ return 0;
+}
---
+++ Data structures/Stack/Stack_Fun.h
@@ -0,0 +1,99 @@
+#ifndef Stack_Fun_H_
+#define Stack_Fun_H_
+#include<stdio.h>
+#define Max_Stack 5
+typedef struct stack {
+    u64 top;
+   u64 arr[Max_Stack]; 
+}Stack;
+void Stack_Ctreate (Stack*PS)
+{
+
+PS-> top = 0 ; 
+
+}
+
+
+u64 isempty (Stack*PS)
+{
+
+if(PS-> top == 0 ) 
+{
+return 1 ;
+}
+else
+{
+    return 0;
+}
+
+}
+
+u64 isfull (Stack*ps)
+{
+
+if(ps-> top == Max_Stack ) 
+{
+return 1 ;
+}
+else
+{
+    return 0;
+}
+}
+
+u64 push (u64 e,Stack*ps)
+{
+if((ps -> top )< Max_Stack )
+{ 
+ps->arr[ps->top]=e ;
+ps->top++;
+return 1 ;
+}
+else
+{
+ return 0;
+}
+
+}
+
+u64 pop(u64 e ,Stack*ps)
+{
+if(ps-> top == 0 )
+{ 
+
+return 0;
+}
+else
+{
+    u64 e=ps->arr[--ps->top];
+    return 1;
+}
+}
+
+void Clear_Stack(Stack*ps)
+{
+ps->top=0;
+}
+
+u64 Stack_Size(Stack*ps)
+{
+    return ps->top ;
+}
+
+void Stack_Display(Stack*ps)
+{
+    printf(">>>>>\n");
+ for(u64 i =0;i< ps->top;i++)
+{
+    printf("%d\n" ,ps->arr[i]);
+}
+   printf("top=%d..." ,ps->top);
+}
+
+
+
+
+
+
+
+#endif /* STDTYPE_H_ */
